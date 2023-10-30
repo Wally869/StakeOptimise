6c472a43d3e8f70ff74a4f70be0019acb463a089
Wally869 <wally869@protonmail.com>
2023-10-30 15:56:34 +0200

    added an 'about' section to he app

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,19 @@
 	import Button from '$lib/components/ui/button/button.svelte';
 
 	import type { Keplr } from '@keplr-wallet/types';
-	import { connect_stargate_client, create_client, get_validator_data, type NetworkSettings, type ReadClient } from '$lib/network-utils';
-	import type { EvalDecentralisationPayload, EvalDecentralisationResponse, StakingResponse, Validator } from '$lib/types/pytypes';
+	import {
+		connect_stargate_client,
+		create_client,
+		get_validator_data,
+		type NetworkSettings,
+		type ReadClient
+	} from '$lib/network-utils';
+	import type {
+		EvalDecentralisationPayload,
+		EvalDecentralisationResponse,
+		StakingResponse,
+		Validator
+	} from '$lib/types/pytypes';
 	import NetworkInfo from '$lib/components/custom/network_info.svelte';
 
 	import { VALIDATORS_DATA } from '$lib/stores/ValidatorsData.js';
@@ -16,7 +27,6 @@
 	import { READ_CLIENT, STARGATE_SIGNING_CLIENT, WRITE_CLIENT } from '$lib/stores/Clients.js';
 
 	let staking_response: StakingResponse | null = null;
-
 
 	let isLocalhost = window.location.hostname === 'localhost';
 
@@ -28,24 +38,21 @@
 		READ_CLIENT.set(await create_client(data.network_settings.rpc));
 		//await connect_stargate_client(network_settings);
 
-
 		if (isLocalhost) {
-			console.log("running in localhost")
+			console.log('running in localhost');
 		}
-		
-		
+
 		try {
 			VALIDATORS_DATA.set(await get_validator_data($READ_CLIENT!));
 		} catch (e) {
-			console.log(e)
+			console.log(e);
 		}
 		/*
 		VALIDATORS_DATA.set(
 			FAKE_VALIDATORS
 		);
 		*/
-		
-		
+
 		// TODO: pass validators data to the api call for eval
 		try {
 			/*
@@ -54,40 +61,34 @@
 				total_supply: 72000
 			}
 			*/
-			let stakes = $VALIDATORS_DATA.map(
-				(val: Validator) => {
-					return val.curr_stake
-				}
-			);
+			let stakes = $VALIDATORS_DATA.map((val: Validator) => {
+				return val.curr_stake;
+			});
 
 			let payload: EvalDecentralisationPayload = {
 				stakes: stakes,
 				total_supply: 0.0
-			}
+			};
 
 			let decentralisation_res = await fetch(
-				"/api/eval", //-decentralisation",
+				'/api/eval', //-decentralisation",
 				{
-					method: "POST",
+					method: 'POST',
 					headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
+						Accept: 'application/json',
+						'Content-Type': 'application/json'
 					},
 					body: JSON.stringify(payload)
 				}
-			)
-
-			let rslt: EvalDecentralisationResponse = await decentralisation_res.json()
-
-			DECENTRALIZATION_RATIO.set(
-				rslt.decentralisation_ratio
 			);
 
-		} catch (e) {
-			console.log(e)
-		}
+			let rslt: EvalDecentralisationResponse = await decentralisation_res.json();
 
-	})
+			DECENTRALIZATION_RATIO.set(rslt.decentralisation_ratio);
+		} catch (e) {
+			console.log(e);
+		}
+	});
 </script>
 
 <svelte:head>
@@ -95,10 +96,14 @@
 </svelte:head>
 
 <div class="bg-primary">
-	<div class="m-auto max-w-4xl h-screen py-16">
-		<div class="grid grid-cols-1 gap-24 place-content-between">
+	<div class="m-auto max-w-4xl h-full py-16">
+		<div class="grid grid-cols-1 gap-12 place-content-between">
 			<!-- // TODO: add query network yield -->
-			<NetworkInfo nb_validators={$VALIDATORS_DATA.length} network_yield={$NETWORK_YIELD} decentralisation_ratio={$DECENTRALIZATION_RATIO} />
+			<NetworkInfo
+				nb_validators={$VALIDATORS_DATA.length}
+				network_yield={$NETWORK_YIELD}
+				decentralisation_ratio={$DECENTRALIZATION_RATIO}
+			/>
 			<div class="w-full">
 				<div
 					class="grid grid-rows gap-8 border border-2 p-8 w-full rounded-lg bg-primary-foreground"
@@ -107,19 +112,28 @@
 						<div class="grid grid-cols-3">
 							<div class="col-span-2">
 								<h1 class="font-semibold text-lg">Stake Optimizer</h1>
-								<p class="text-gray-400">Maximize yield when staking and help decentralise governance</p>
+								<p class="text-gray-400">
+									Maximize yield when staking and help decentralise governance
+								</p>
 							</div>
 							<div class="col-span-1 grid place-items-end">
 								<div class="pb-4">
 									{#if $STARGATE_SIGNING_CLIENT}
-										<Button class="" variant="outline" on:click={() => {STARGATE_SIGNING_CLIENT.set(null)}}
-											>Disconnect</Button
+										<Button
+											class=""
+											variant="outline"
+											on:click={() => {
+												STARGATE_SIGNING_CLIENT.set(null);
+											}}>Disconnect</Button
 										>
 									{:else}
-										<Button class="" on:click={async () => {
-											//WRITE_CLIENT.set(await connect_stargate_client(network_settings))
-											await connect_stargate_client(network_settings);
-										}}>Connect Wallet</Button>
+										<Button
+											class=""
+											on:click={async () => {
+												//WRITE_CLIENT.set(await connect_stargate_client(network_settings))
+												await connect_stargate_client(network_settings);
+											}}>Connect Wallet</Button
+										>
 									{/if}
 								</div>
 							</div>
@@ -129,6 +143,49 @@
 
 					<div class="w-full">
 						<StakeParametersInput />
+					</div>
+				</div>
+			</div>
+			<div class="w-full">
+				<div
+					class="border border-2 p-8 w-full rounded-lg bg-primary-foreground"
+				>
+					<h1 class="font-semibold text-lg pb-4">About Stake Optimizer</h1>
+					<div class="text-gray-800 grid grid-rows gap-4">
+						<p>
+							In Proof of Stake (PoS) cryptocurrency networks, it's crucial to evenly distribute
+							stakes among validators for several key reasons:
+						</p>
+						<ul class="grid grid-rows gap-2">
+							<li>
+								- Security: A balanced stake distribution reduces the risk of centralization,
+								enhancing network security.
+							</li>
+							<li>
+								- Decentralization: Equal stakes support the network's goal of avoiding control by a
+								single entity or small group.
+							</li>
+							<li>
+								- Preventing Cartels: Balanced stakes discourage validator cartels that can undermine
+								fair competition.
+							</li>
+							<li>
+								- Incentives: Equal distribution encourages more validators to participate, increasing
+								network resilience.
+							</li>
+							<li>
+								- Resilience: A balanced distribution makes the network more resistant to targeted
+								attacks.
+							</li>
+							<li>
+								- Trustworthiness: Users trust networks that distribute stakes fairly, fostering user
+								confidence.
+							</li>
+						</ul>
+
+						<p>
+							StakeOptimizer helps strengthen the network by splitting your delegations between validators to approach an equal staking value for all validators.
+						</p>
 					</div>
 				</div>
 			</div>
